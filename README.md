52479f619b60abb8e277629fd22b8ffeb1859296
Kizuna-AI0630-MAC <andy040608@hotmail.com>
2026-03-22 14:33:03 -0400

    templete update

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-# Group-Project
+## 431W Nitanny Auction Project
 2026 Spring, cmpsc 431w, 小组 刘轩，刘金旸，王皓，李思蕊
-
-# Webpage design
-
-Webpage design is located at the templates folder
 
 # Do not modfy files in he Dataset folder
 
 # venv is the python enviroment
+
+## Features
+
+## Organization
+1. All webpage design in located at the templates folder.
+
+## Instructions
+1. Open the code folder in VS code
+2. Run **pip install flask**
+3. Run **python app.py**
+4. Click ***http://127.0.0.1:5000/*** in terminal
+
+## Reference
--- auction_app.py
+++ auction_app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 host = 'http://127.0.0.1:5000/'
 
-# Handle the form submission from index.html
 @app.route('/navigate', methods=['POST'])
 def navigate():
     action = request.form.get('actions') # Get the selected value from the dropdown menu
@@ -15,22 +14,6 @@ def navigate():
     elif action == 'delete':
         return redirect(url_for('delete_patient'))
     return redirect(url_for('index'))
-
-
-@app.route('/delete', methods=['GET', 'POST']) # Delete patient name page
-def delete_patient():
-    error = None
-    if request.method == 'POST':
-        rows_deleted = delete_patient_name(request.form['FirstName'], request.form['LastName'])
-        if rows_deleted == 0:
-            error = 'Patient not found'
-
-    result = get_all_patients()
-    return render_template('delete_patient.html', error=error, result=result)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 @app.route('/add', methods=['POST', 'GET']) # Add patient name page
@@ -53,20 +36,6 @@ def valid_name(first_name, last_name): # Helper function to input name and add i
     connection.commit()
     cursor = connection.execute('SELECT * FROM users;')
     return cursor.fetchall()
-
-def get_all_patients(): # Helper function to get history data
-    connection = sql.connect('database.db')
-    connection.execute('CREATE TABLE IF NOT EXISTS users(pid INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT);') # modify to automatically generated pid
-    cursor = connection.execute('SELECT * FROM users;')
-    data = cursor.fetchall()
-    return data
-
-def delete_patient_name(first_name, last_name): # Helper function to delete a patient from the database
-    connection = sql.connect('database.db')
-    cursor = connection.execute('DELETE FROM users WHERE firstname=? AND lastname=?;', (first_name, last_name))
-    rows_affected = cursor.rowcount
-    connection.commit()
-    return rows_affected
 
 if __name__ == "__main__":
     app.run()
